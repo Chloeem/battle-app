31f731a25db89a5536e1fd3f7383b2d7c7a45745
Chloe <chloeemstokes@gmail.com>
2022-02-08 18:10:45 +0000

    Enter players passing all tests

--- app.rb
+++ app.rb
@@ -11,9 +11,14 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    p params
     @player_1 = params[:player_1]
     @player_2 = params[:player_2]
-    erb(:index)
+    erb(:play)
   end
 
   run! if app_file == $0
